2b87b3ba0c699dfe9e3e64158f56b5ee09045595
Oluwakeji <onabajooluwakeji.daniel@gmail.com>
2022-08-14 14:59:09 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [result, setResult] = useState('');
+
+  const handleClick = (e) => {
+    setResult(result + e.target.value);
+    
+    if (result.slice(0, 1) === '=') {
+      setResult(e.target.value);
+    }else if(result === 'Error'){
+      setResult(e.target.value);
+    }
+  }
+
+  const handleTyping = (e) => {
+    setResult(e.target.value);
+  }
+
+  const del = () => {
+    setResult(result.slice(0, -1));
+  }
+
+  const clear = () => {
+    setResult('');
+  }
+
+  const calculate = () => {
+    try {
+      setResult('= ' + eval(result).toString());
+    } catch (error) {
+      setResult('Error');
+    }
+   
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form action=''>
+        <input type='text' value={result} onChange={handleTyping} />
+      </form>
+
+      <div className='keypad' >
+      <button className='highlight' onClick={clear} id='clear' >Clear</button>
+      <button className='highlight' onClick={del} id='del' >Del</button>
+      <button className='highlight' onClick={handleClick} value='/'>&divide;</button>
+      <button onClick={handleClick} value='7'>7</button>
+      <button onClick={handleClick} value='8'>8</button>
+      <button onClick={handleClick} value='9'>9</button>
+      <button className='highlight' onClick={handleClick} value='*'>&times;</button>
+      <button onClick={handleClick} value='4'>4</button>
+      <button onClick={handleClick} value='5'>5</button>
+      <button onClick={handleClick} value='6'>6</button>
+      <button className='highlight' onClick={handleClick} value='-'>&ndash;</button>
+      <button onClick={handleClick} value='1'>1</button>
+      <button onClick={handleClick} value='2'>2</button>
+      <button onClick={handleClick} value='3'>3</button>
+      <button className='highlight' onClick={handleClick} value='+'>+</button>
+      <button onClick={handleClick} value='0'>0</button>
+      <button onClick={handleClick} value='.'>.</button>
+      <button className='highlight' onClick={calculate} id='result' >=</button>
+      </div>     
     </div>
   );
 }
